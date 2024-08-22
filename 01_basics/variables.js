4ff30368acb0221fb4c5b53bf9c2da9d615624ee
Mohit Yadav <99004828+mont-y-001@users.noreply.github.com>
2024-08-22 20:49:05 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+x = 4
+y = 5
+z = x+y
+console.log(z) 
+
+//Let,var and const
+//var is a keyword used only for written code in older browsers
+//Let and consgt are mainly used now
+
+const number = 32
+let EmailId = "my266620@gmail.com"
+var accountId = "mohity8237" 
+console.table([number,EmailId,accountId])
